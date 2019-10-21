45913edf99f1ea426bed0e8ab97b43dab08fa58e
Yury Kudryavtsev <kudryavtsev@greensight.ru>
2019-10-21 16:51:44 +0300

    #50746 Modal message revert

---
+++ html/src/vue/components/modal-message/modal-message.vue
@@ -0,0 +1,24 @@
+<template>
+    <transition name="modal">
+        <modal :close="closeModal" v-if="isModalOpen('messageBox')">
+            <div slot="header">
+                <p v-html="$store.state.modal.messageBox.title"></p>
+            </div>
+            <div slot="body">
+                <p v-html="$store.state.modal.messageBox.text"></p>
+            </div>
+        </modal>
+    </transition>
+</template>
+
+<script>
+    import modal from '../controls/modal/modal.vue';
+    import modalMixin from '../../mixins/modal.js';
+
+    export default {
+        mixins: [modalMixin],
+        components: {
+            modal,
+        },
+    };
+</script>
