1033b96fd4e9fcc2bb8db245428a6b46aa442309
Yury Kudryavtsev <kudryavtsev@greensight.ru>
2020-01-27 10:40:16 +0300

    #52603 FlowDeliveryController additions

--- app/Http/Controllers/Orders/Flow/FlowDeliveryController.php
+++ app/Http/Controllers/Orders/Flow/FlowDeliveryController.php
@@ -44,14 +44,15 @@ class FlowDeliveryController extends Controller
 
 
     /**
-     * @param int $id ID доставки
+     * Получить доставку
+     * @param int $deliveryId ID доставки
      * @return DeliveryDto|null
      */
-    protected function loadDelivery(int $id):? DeliveryDto
+    protected function loadDelivery(int $deliveryId):? DeliveryDto
     {
         $deliveryService = resolve(DeliveryService::class);
         try {
-            $delivery = $deliveryService->delivery($id);
+            $delivery = $deliveryService->delivery($deliveryId);
         } catch (Exception $e) {}
 
         if(isset($delivery)) {
@@ -62,19 +63,20 @@ class FlowDeliveryController extends Controller
     }
 
     /**
-     * @param int $id ID доставки
+     * Получить отправления доставки
+     * @param int $deliveryId ID доставки
      * @return Collection|null
      */
-    protected function loadShipments(int $id):? Collection
+    protected function loadShipments(int $deliveryId):? Collection
     {
         $shipmentService = resolve(ShipmentService::class);
-        $shipmentQuery = $shipmentService
+        $restQuery = $shipmentService
             ->newQuery()
-            ->setFilter('delivery_id', $id)
-            ->include('basketItems', 'items', 'packages.items');
+            ->setFilter('delivery_id', $deliveryId)
+            ->include('basketItems', 'items', 'packages.items', 'cargo');
 
         try {
-            $shipments = $shipmentService->shipments($shipmentQuery);
+            $shipments = $shipmentService->shipments($restQuery);
         } catch (Exception $e) {}
 
         if(isset($shipments)) {
@@ -83,5 +85,4 @@ class FlowDeliveryController extends Controller
 
         return null;
     }
-
 }
