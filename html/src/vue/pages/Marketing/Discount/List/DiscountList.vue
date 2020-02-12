39c8997e20d7dab538300a6aed48be24509405d5
Kapitanov Andrey <andrey@kapdx.ru>
2020-02-12 18:41:10 +0300

    #55810 список и создание скидки

--- html/src/vue/pages/Marketing/Discount/List/DiscountList.vue
+++ html/src/vue/pages/Marketing/Discount/List/DiscountList.vue
@@ -10,37 +10,61 @@
             </div>
             <div class="card-body">
                 <div class="row">
-                    <f-input v-model="filter.id" class="col">
+                    <f-input v-model="filter.id" class="col-2" type="number">
                         ID
                     </f-input>
-                    <f-input v-model="filter.name" class="col">
+                    <f-input v-model="filter.name" class="col-4">
                         Название
                     </f-input>
-                    <f-input v-model="filter.promo_code" class="col">
-                        Промокод
-                    </f-input>
+                    <f-multi-select v-model="filter.status"
+                                    :options="discountStatusesOptions"
+                                    :name="'status'"
+                                    class="col-3">
+                        Статус скидки
+                    </f-multi-select>
+                    <f-multi-select v-model="filter.approval_status"
+                                    :options="discountApprovalStatusesOptions"
+                                    :name="'approval_status'"
+                                    class="col-3">
+                        Статус проверки
+                    </f-multi-select>
                 </div>
                 <transition name="slide">
-                    <div v-if="opened">
+                    <div v-show="opened">
                         <div class="additional-filter pt-3 mt-3">
                             <div class="row mt-3">
-                                <f-multi-select v-model="filter.type" :options="discountTypesOptions" class="col">
-                                    Тип
+                                <f-multi-select v-model="filter.type"
+                                                :options="discountTypesOptions"
+                                                :name="'type'"
+                                                class="col-6">
+                                    Скидка на
                                 </f-multi-select>
-                                <f-multi-select v-model="filter.status" :options="discountStatusesOptions" class="col">
-                                    Статус скидки
-                                </f-multi-select>
-                                <f-multi-select v-model="filter.approval_status" :options="discountApprovalStatusesOptions" class="col">
-                                    Статус проверки
-                                </f-multi-select>
+
+                                <v-select v-model="filter.role_id" :options="roles" class="col-6">Роль</v-select>
                             </div>
                             <div class="row mt-3">
-                                <f-date v-model="filter.start_date" class="col-2">
+                                <f-date v-model="filter.start_date" class="col-3">
                                     Начало действия скидки
                                 </f-date>
-                                <f-date v-model="filter.end_date" class="col-2">
+                                <div class="col-3">
+                                    <label>Точная дата
+                                        <fa-icon icon="question-circle" v-b-popover.hover="fixDateTooltip"></fa-icon>
+                                    </label>
+                                    <div>
+                                        <input class="ml-5" type="checkbox" v-model="filter.fix_start_date">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <f-date v-model="filter.end_date" class="col-3">
                                     Конец действия скидки
                                 </f-date>
+                                <div class="col-3">
+                                    <label>&nbsp;</label>
+                                    <div>
+                                        <input class="ml-5" type="checkbox" v-model="filter.fix_end_date">
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -51,15 +75,20 @@
                 <button @click="clearFilter" class="btn btn-sm btn-outline-dark">Очистить</button>
             </div>
         </div>
-        <div>
-            <a :href="getRoute('discount.create')" class="btn btn-success mt-3">Создать скидку</a>
+
+        <div class="row mb-3">
+            <div class="col-12">
+                <a :href="getRoute('discount.create')" class="btn btn-success mt-3">Создать скидку</a>
+            </div>
         </div>
-        <table class="table table-condensed">
-            <thead>
+
+        <table class="table table-hover">
+            <thead class="thead-light">
             <tr>
                 <th>ID</th>
-                <th>Тип</th>
-                <th>Имя</th>
+                <th>Название</th>
+                <th>Скидка</th>
+                <th>Срок действия</th>
                 <th>Статус проверки</th>
                 <th>Статус скидки</th>
                 <th></th>
@@ -71,17 +100,16 @@
             </tr>
             <tr v-if="discounts" v-for="(discount, index) in discounts">
                 <td>{{ discount.id }}</td>
-                <td>{{ discountTypes[discount.type].name }}</td>
                 <td>{{ discount.name }}</td>
-                <td>
-                    <span class="badge" :class="approvalClass(discount)">
-                        {{ discount.approvalStatusName }}
-                    </span>
+                <td>{{ discount.value }}{{ discount.value_type === DISCOUNT_VALUE_TYPE_RUB ? ' руб.' : '%' }} на<br/>
+                    <b>{{ discountTypeName(discount.type) }}</b>
                 </td>
-                <td>
-                    <span class="badge" :class="statusClass(discount)">
-                        {{ discount.statusName }}
-                    </span>
+                <td>{{ discount.validityPeriod }}</td>
+                <td :class="approvalClass(discount)">
+                    <span class="badge">{{ discount.approvalStatusName }}</span>
+                </td>
+                <td :class="statusClass(discount)">
+                    <span class="badge">{{ discount.statusName }}</span>
                 </td>
                 <td>
                     <a :href="getRoute('discount.edit', {id: discount.id})" class="btn btn-info">
@@ -92,11 +120,10 @@
             </tbody>
         </table>
         <b-pagination
-                v-if="pager.pages > 1"
+                v-if="total > iPager.perPage"
                 v-model="currentPage"
-                :total-rows="pager.total"
-                :per-page="pager.pageSize"
-                :hide-goto-end-buttons="pager.pages < 10"
+                :total-rows="total"
+                :per-page="iPager.perPage"
                 @change="changePage"
         ></b-pagination>
     </layout-main>
@@ -107,136 +134,217 @@
     import withQuery from 'with-query';
     import qs from 'qs';
     import {mapGetters} from 'vuex';
+    import FCheckbox from '../../../../components/filter/f-checkbox.vue';
     import FInput from '../../../../components/filter/f-input.vue';
+    import VSelect from '../../../../components/controls/VSelect/VSelect.vue';
     import FMultiSelect from '../../../../components/filter/f-multi-select.vue';
     import FDate from '../../../../components/filter/f-date.vue';
 
-    const cleanHiddenFilter = {
-    type: [],
-    status: [],
-    approval_status: [],
-    start_date: '',
-    end_date: '',
-};
+    const cleanFilter = {
+        id: '',
+        name: '',
+        type: [],
+        status: [],
+        approval_status: [],
+        start_date: '',
+        end_date: '',
+        fix_start_date: false,
+        fix_end_date: false,
+        role_id: null,
+    };
 
-const cleanFilter = Object.assign({
-    id: '',
-    name: '',
-    promo_code: '',
-}, cleanHiddenFilter);
+    export default {
+        name: 'page-discounts-list',
+        components: {
+            FInput,
+            VSelect,
+            FMultiSelect,
+            FCheckbox,
+            FDate,
+        },
+        props: {
+            iDiscounts: [Array, null],
+            iCurrentPage: Number,
+            discountTypes: Object,
+            discountStatuses: Object,
+            discountApprovalStatuses: Object,
+            iPager: Object,
+            roles: Array,
+            iFilter: Object|Array,
+        },
+        data() {
+            return {
+                total: 0,
+                currentPage: this.iCurrentPage,
+                discounts: this.iDiscounts,
+                filter: {},
+                options: [],
+                appliedFilter: {},
+                opened: false,
 
-export default {
-    name: 'page-discounts-list',
-    components: {
-        FInput,
-        FMultiSelect,
-        FDate,
-    },
-    props: {
-        iDiscounts: [Array, null],
-        iCurrentPage: Number,
-        discountTypes: Object,
-        discountStatuses: Object,
-        discountApprovalStatuses: Object,
-        pager: Object
-    },
-    data() {
-        return {
-            currentPage: this.iCurrentPage,
-            discounts: this.iDiscounts,
-            filter: {},
-            options: [],
-            appliedFilter: {},
-            opened: false,
-        };
-    },
-    methods: {
-        changePage(newPage) {
-            history.pushState(null, null, location.origin + location.pathname + withQuery('', {
-                page: newPage,
-                filter: this.appliedFilter,
-            }));
+                // Статус заявки
+                APP_STATUS_NOT_APPROVED: 1,
+                APP_STATUS_SENT: 2,
+                APP_STATUS_APPROVING: 3,
+                APP_STATUS_REJECT: 4,
+                APP_STATUS_APPROVED: 5,
+
+                // Статус скидки
+                STATUS_ACTIVE: 1,
+                STATUS_PAUSED: 2,
+                STATUS_EXPIRED: 3,
+
+                // Тип значения
+                DISCOUNT_VALUE_TYPE_PERCENT: 1,
+                DISCOUNT_VALUE_TYPE_RUB: 2,
+            };
         },
-        loadPage() {
-            Service.net().get(this.route('discount.pagination'), {
-                page: this.currentPage,
-                filter: this.appliedFilter,
-            }).then(data => {
-                this.discounts = data.iDiscounts;
-            });
-        },
-        approvalClass(discount) {
-            switch (discount.approval_status) {
-                case 1: return 'badge-dark';
-                case 2: return 'badge-info';
-                case 3: return 'badge-warning';
-                case 4: return 'badge-danger';
-                case 5: return 'badge-success';
-            }
-        },
-        statusClass(discount) {
-            switch (discount.status) {
-                case 1: return 'badge-success';
-                case 2: return 'badge-warning';
-                case 3: return 'badge-dark';
-            }
-        },
-        applyFilter() {
-            let tmpFilter = {};
-            for (let [key, value] of Object.entries(this.filter)) {
-                if (value && Object.keys(cleanFilter).indexOf(key) !== -1) {
-                    tmpFilter[key] = value;
+        methods: {
+            changePage(newPage) {
+                history.pushState(null, null, location.origin + location.pathname + withQuery('', {
+                    page: newPage,
+                    filter: this.appliedFilter,
+                }));
+            },
+            loadPage() {
+                Service.net().get(this.route('discount.pagination'), {
+                    page: this.currentPage,
+                    filter: this.appliedFilter,
+                }).then(data => {
+                    this.discounts = data.iDiscounts;
+                    this.total = data.total;
+                });
+            },
+            approvalClass(discount) {
+                switch (discount.approval_status) {
+                    case this.APP_STATUS_NOT_APPROVED:
+                        return 'table-danger';
+                    case this.APP_STATUS_SENT:
+                        return 'table-info';
+                    case this.APP_STATUS_APPROVING:
+                        return 'table-warning';
+                    case this.APP_STATUS_REJECT:
+                        return 'table-danger';
+                    case this.APP_STATUS_APPROVED:
+                        return 'table-success';
                 }
-            }
-            this.appliedFilter = tmpFilter;
-            this.currentPage = 1;
-            this.changePage(1);
-            this.loadPage();
-        },
-        toggleHiddenFilter() {
-            this.opened = !this.opened;
-            if (this.opened === false) {
-                for (let entry of Object.entries(cleanHiddenFilter)) {
+            },
+            statusClass(discount) {
+                switch (discount.status) {
+                    case this.STATUS_ACTIVE:
+                        return 'table-success';
+                    case this.STATUS_PAUSED:
+                        return 'table-warning';
+                    case this.STATUS_EXPIRED:
+                        return 'table-secondary';
+                }
+            },
+            applyFilter() {
+                let tmpFilter = {};
+                for (let [key, value] of Object.entries(this.filter)) {
+                    if (value && Object.keys(cleanFilter).indexOf(key) !== -1) {
+                        tmpFilter[key] = value;
+                    }
+                }
+                this.appliedFilter = tmpFilter;
+                this.currentPage = 1;
+                this.changePage(1);
+                this.loadPage();
+            },
+            toggleHiddenFilter() {
+                this.opened = !this.opened;
+            },
+            clearFilter() {
+                for (let entry of Object.entries(cleanFilter)) {
                     this.filter[entry[0]] = JSON.parse(JSON.stringify(entry[1]));
                 }
+                Service.event().$emit('clear-filter');
                 this.applyFilter();
-            }
-        },
-        clearFilter() {
-            for (let entry of Object.entries(cleanFilter)) {
-                this.filter[entry[0]] = JSON.parse(JSON.stringify(entry[1]));
-            }
-            this.applyFilter();
-        }
-    },
-    computed: {
-        discountTypesOptions() {
-            return Object.values(this.discountTypes).map(type => ({value: type.id, text: type.name}));
-        },
-        discountStatusesOptions() {
-            return Object.values(this.discountStatuses).map(type => ({value: type.id, text: type.name}));
-        },
-        discountApprovalStatusesOptions() {
-            return Object.values(this.discountApprovalStatuses).map(type => ({value: type.id, text: type.name}));
-        },
+            },
+            discountTypeName(type) {
+                return (type in this.discountTypes) ? this.discountTypes[type].name : 'N/A';
+            },
+            initFilter() {
+                let fields = [
+                    'id',
+                    'name',
+                    'type',
+                    'status',
+                    'approval_status',
+                    'start_date',
+                    'end_date',
+                    'fix_start_date',
+                    'fix_end_date',
+                    'role_id',
+                ];
 
-        ...mapGetters(['getRoute'])
-    },
-    created() {
-        window.onpopstate = () => {
-            let query = qs.parse(document.location.search.substr(1));
-            if (query.page) {
-                this.currentPage = query.page;
+                let filter = {};
+                for (let i in fields) {
+                    let field = fields[i];
+                    if (!(field in this.iFilter)) {
+                        continue;
+                    }
+
+                    this.opened = this.opened || ['type', 'start_date', 'end_date', 'role_id'].includes(field);
+
+                    switch (field) {
+                        case 'id':
+                        case 'name':
+                        case 'start_date':
+                        case 'end_date':
+                        case 'fix_start_date':
+                        case 'fix_end_date':
+                        case 'role_id':
+                            filter[field] = this.iFilter[field];
+                            break;
+                        case 'type':
+                        case 'status':
+                        case 'approval_status':
+                            let value = this.iFilter[field].map(v => parseInt(v));
+                            filter[field] = value;
+                            Service.event().$emit('set-filter-' + field, value);
+                            break;
+                    }
+                }
+
+                this.filter = filter;
+            },
+        },
+        computed: {
+            discountTypesOptions() {
+                return Object.values(this.discountTypes).map(type => ({value: type.id, text: type.name}));
+            },
+            discountStatusesOptions() {
+                return Object.values(this.discountStatuses).map(type => ({value: type.id, text: type.name}));
+            },
+            discountApprovalStatusesOptions() {
+                return Object.values(this.discountApprovalStatuses).map(type => ({value: type.id, text: type.name}));
+            },
+            fixDateTooltip() {
+                return 'Искать точное совпадание с датой начала и/или окончания скидки';
+            },
+            ...mapGetters(['getRoute'])
+        },
+        mounted() {
+            this.initFilter();
+            this.appliedFilter = this.filter;
+        },
+        created() {
+            this.total = this.iPager.total;
+            window.onpopstate = () => {
+                let query = qs.parse(document.location.search.substr(1));
+                if (query.page) {
+                    this.currentPage = query.page;
+                }
+            };
+            // this.opened = this.isHiddenFilterDefaultOpen();
+        },
+        watch: {
+            currentPage() {
+                this.loadPage();
             }
-        };
-        // this.opened = this.isHiddenFilterDefaultOpen();
-    },
-    watch: {
-        currentPage() {
-            this.loadPage();
         }
-    }
-};
+    };
 </script>
 <style scoped>
     .additional-filter {
