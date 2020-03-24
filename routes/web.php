3148669f4a9e941a6192dde47bb7646109c057bd
Evgeny Bereza <bereza@greensight.ru>
2020-03-24 17:16:33 +0300

    #58015 Готов список логистических операторов

--- routes/web.php
+++ routes/web.php
@@ -270,6 +270,21 @@ Route::middleware('auth')->group(function () {
     });
 
     Route::prefix('logistics')->namespace('Logistics')->group(function () {
+        Route::prefix('delivery-services')->namespace('DeliveryService')->group(function () {
+            Route::get('/', 'DeliveryServiceListController@index')->name('deliveryService.list');
+            Route::get('/page', 'DeliveryServiceListController@page')->name('deliveryService.pagination');
+
+            Route::prefix('/{id}')->where(['id' => '[0-9]+'])->group(function () {
+                Route::get('', 'DeliveryServiceDetailController@index')->name('deliveryService.detail');
+
+                Route::namespace('Detail')->group(function () {
+                    Route::prefix('settings')->group(function () {
+                        Route::get('', 'TabSettingsController@load')->name('deliveryService.detail.settings');
+                    });
+                });
+            });
+        });
+
         Route::prefix('delivery-prices')->group(function () {
             Route::get('/', 'DeliveryPriceController@index')->name('deliveryPrice.index');
             Route::put('/delivery-price', 'DeliveryPriceController@save')->name('deliveryPrice.save');
