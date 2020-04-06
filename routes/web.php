1823e7100334a88b987ad82337944593d8a20d2c
Anton Filippov <filippovano.work@yandex.ru>
2020-04-06 22:53:12 +0300

    #57921 wip Добавлена кнопка Сохранить в деталке склада.
    Переделана управляющая логика.

--- routes/web.php
+++ routes/web.php
@@ -285,6 +285,8 @@ Route::middleware('auth')->group(function () {
                     ->name('merchantStore.deleteContact');
             });
 
+            Route::get('/pickup-times', 'MerchantStoreController@pickupTime')
+                ->name('merchantStore.pickupTime');
             Route::put('/pickup-times', 'MerchantStoreController@savePickupTime')
                 ->name('merchantStore.savePickupTime');
         });
