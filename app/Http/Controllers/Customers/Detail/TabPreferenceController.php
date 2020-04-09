ac9d9dc41bc0dadf54e3646cb2d96ad39a41a63b
Boris Sonkin <sonkin@greensight.ru>
2020-04-09 22:53:14 +0300

    #57129 исправление логики в связи с реорганизацией структуры предпочтений

--- app/Http/Controllers/Customers/Detail/TabPreferenceController.php
+++ app/Http/Controllers/Customers/Detail/TabPreferenceController.php
@@ -40,8 +40,8 @@ class TabPreferenceController extends Controller
             'brands' => $brands->keyBy('id'),
             'categories' => $categories->keyBy('id'),
             'customer' => [
-                'brands' => $customer->brands,
-                'categories' => $customer->categories,
+                'brands' => $customer->own_brands,
+                'categories' => $customer->own_categories,
             ],
             'favorites' => $favoriteItems ? $this->loadFavoriteItems($query, $productService) : null,
         ]);
@@ -89,7 +89,8 @@ class TabPreferenceController extends Controller
             'brands.*' => 'numeric',
         ]);
 
-        $customerService->updateBrands($id, request('brands'));
+        //TODO: Не забыть переделать в #57176
+        $customerService->updateBrands($id, 1, request('brands'));
 
         return response('', 204);
     }
@@ -101,7 +102,8 @@ class TabPreferenceController extends Controller
             'categories.*' => 'numeric',
         ]);
 
-        $customerService->updateCategories($id, request('categories'));
+        //TODO: Не забыть переделать в #57176
+        $customerService->updateCategories($id, 1, request('categories'));
 
         return response('', 204);
     }
