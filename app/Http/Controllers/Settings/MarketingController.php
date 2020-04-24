f3c7c27754bdebccf9bef6ab9b2837be904dd6b5
Kapitanov Andrey <andrey@kapdx.ru>
2020-04-24 14:42:46 +0300

    #55297 Настройки маркетинговых инструментов

--- app/Http/Controllers/Settings/MarketingController.php
+++ app/Http/Controllers/Settings/MarketingController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Greensight\CommonMsa\Dto\UserDto;
 use Greensight\Marketing\Dto\Option\OptionDto;
 use Greensight\Marketing\Services\OptionService\OptionService;
+use Illuminate\Validation\Rule;
 
 
 class MarketingController extends Controller
@@ -18,6 +19,9 @@ class MarketingController extends Controller
         return $this->render('Settings/Marketing', [
             'bonus_per_rubles' => $optionService->get(OptionDto::KEY_BONUS_PER_RUBLES),
             'roles_available_for_bonuses' => $optionService->get(OptionDto::KEY_ROLES_AVAILABLE_FOR_BONUSES),
+            'activation_bonus_name' => $optionService->get(OptionDto::KEY_ACTIVATION_BONUS_NAME),
+            'activation_bonus_value' => $optionService->get(OptionDto::KEY_ACTIVATION_BONUS_VALUE),
+            'activation_bonus_valid_period' => $optionService->get(OptionDto::KEY_ACTIVATION_BONUS_VALID_PERIOD),
 
             'roles' => Helpers::getOptionRoles(false),
         ]);
@@ -26,26 +30,36 @@ class MarketingController extends Controller
     public function update(OptionService $optionService)
     {
         $data = $this->validate(request(), [
-            'bonus_per_rubles' => 'numeric',
+            'bonus_per_rubles' => 'numeric|gte:0',
             'roles_available_for_bonuses'  => 'array',
-            'roles_available_for_bonuses.*'  => 'integer',
+            'roles_available_for_bonuses.*'  => [
+                Rule::in([
+                    UserDto::SHOWCASE__PROFESSIONAL,
+                    UserDto::SHOWCASE__REFERRAL_PARTNER
+                ])],
+            'activation_bonus_name'  => 'string|nullable',
+            'activation_bonus_value'  => 'integer|gt:0|nullable',
+            'activation_bonus_valid_period'  => 'integer|gte:0|nullable',
         ]);
 
-        if (array_key_exists('bonus_per_rubles', $data)) {
-            $bonusPerRub = (float) $data['bonus_per_rubles'];
-            if ($bonusPerRub >= 0) {
-                $optionService->put(OptionDto::KEY_BONUS_PER_RUBLES, $bonusPerRub);
+        foreach ($data as $key => $v) {
+            switch ($key) {
+                case 'bonus_per_rubles':
+                    $optionService->put(OptionDto::KEY_BONUS_PER_RUBLES, (float) $v);
+                    break;
+                case 'roles_available_for_bonuses':
+                    $optionService->put(OptionDto::KEY_ROLES_AVAILABLE_FOR_BONUSES, $v);
+                    break;
+                case 'activation_bonus_name':
+                    $optionService->put(OptionDto::KEY_ACTIVATION_BONUS_NAME, $v);
+                    break;
+                case 'activation_bonus_value':
+                    $optionService->put(OptionDto::KEY_ACTIVATION_BONUS_VALUE, $v ? (int) $v : null);
+                    break;
+                case 'activation_bonus_valid_period':
+                    $optionService->put(OptionDto::KEY_ACTIVATION_BONUS_VALID_PERIOD, $v ? (int) $v : null);
+                    break;
             }
-        }
-        if (array_key_exists('roles_available_for_bonuses', $data)) {
-            $roles = array_filter($data['roles_available_for_bonuses'], function ($roleId) {
-                return in_array($roleId, [
-                    UserDto::SHOWCASE__PROFESSIONAL,
-                    UserDto::SHOWCASE__REFERRAL_PARTNER,
-                ]);
-            });
-
-            $optionService->put(OptionDto::KEY_ROLES_AVAILABLE_FOR_BONUSES, $roles);
         }
     }
 }
--- html/src/vue/pages/Settings/Marketing/Marketing.vue
+++ html/src/vue/pages/Settings/Marketing/Marketing.vue
@@ -20,6 +20,52 @@
                 </f-multi-select>
             </div>
 
+            <h4 class="mb-3 mt-3">Бонусы за активацию клиента</h4>
+            <div class="">
+                <span class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="bonus-activation-btn" key="bonusActivationBtn" v-model="bonusActivationBtn">
+                    <label class="custom-control-label" for="bonus-activation-btn"></label>
+                    <label for="bonus-activation-btn">Автоматически начислять бонусы при активации клиента</label>
+                </span>
+            </div>
+
+            <div class="mb-3 row" v-if="bonusActivationBtn">
+                <v-input v-model="$v.form.activation_bonus_name.$model"
+                    :error="errorActivationBonusName"
+                    class="col-6"
+                    placeholder="Бонус за активацию"
+                    @change="() => {updateInput('activation_bonus_name')}"
+                >Название события</v-input>
+                <v-input v-model="$v.form.activation_bonus_value.$model"
+                     :error="errorActivationBonusValue"
+                     class="col-6"
+                     type="number"
+                     min="1"
+                     @change="() => {updateInput('activation_bonus_value')}"
+                >Сумма</v-input>
+                <div class="col-12">
+                    <span class="custom-control custom-switch">
+                        <input type="checkbox"
+                            class="custom-control-input"
+                            id="bonus-unlimited-period-btn"
+                            key="bonusUnlimitedPeriodBtn"
+                            v-model="bonusUnlimitedPeriodBtn"
+                        >
+                        <label class="custom-control-label" for="bonus-unlimited-period-btn"></label>
+                        <label for="bonus-unlimited-period-btn">Неограниченный срок действия бонусов</label>
+                    </span>
+                </div>
+                <v-input v-model="$v.form.activation_bonus_valid_period.$model"
+                         v-if="!bonusUnlimitedPeriodBtn"
+                         :error="errorActivationBonusValidPeriod"
+                         class="col-3"
+                         type="number"
+                         min="1"
+                         @change="() => {updateInput('activation_bonus_valid_period')}"
+                >Срок действия (дней)</v-input>
+
+            </div>
+
             <div class="form-group mt-3">
                 <button type="submit" class="btn btn-primary">Сохранить</button>
             </div>
@@ -31,7 +77,7 @@
 <script>
     import Services from '../../../../scripts/services/services';
     import {validationMixin} from 'vuelidate';
-    import {required, minValue} from 'vuelidate/lib/validators';
+    import {required, requiredIf, minValue, integer} from 'vuelidate/lib/validators';
     import VInput from '../../../components/controls/VInput/VInput.vue';
     import FMultiSelect from '../../../components/filter/f-multi-select.vue';
 
@@ -41,6 +87,9 @@
         props: {
             bonus_per_rubles: Number,
             roles_available_for_bonuses: Array,
+            activation_bonus_name: [String, null],
+            activation_bonus_value: [Number, null],
+            activation_bonus_valid_period: [Number, null],
             roles: Array,
         },
         mixins: [validationMixin],
@@ -49,13 +98,31 @@
                 form: {
                     bonus_per_rubles: this.bonus_per_rubles,
                     roles_available_for_bonuses: this.roles_available_for_bonuses,
+                    activation_bonus_name: this.activation_bonus_name,
+                    activation_bonus_value: this.activation_bonus_value,
+                    activation_bonus_valid_period: this.activation_bonus_valid_period,
                 },
                 requestData: {},
+                bonusActivationBtn: this.activation_bonus_value > 0,
+                bonusUnlimitedPeriodBtn: this.activation_bonus_valid_period <= 0,
             }
         },
         validations: {
             form: {
                 bonus_per_rubles: {required, minValue: minValue(0)},
+                activation_bonus_name: {
+                    required: requiredIf(function () { return this.bonusActivationBtn }),
+                },
+                activation_bonus_value: {
+                    required: requiredIf(function () { return this.bonusActivationBtn }),
+                    minValue: minValue(1),
+                    integer
+                },
+                activation_bonus_valid_period: {
+                    required: requiredIf(function () { return this.bonusActivationBtn && !this.bonusUnlimitedPeriodBtn }),
+                    minValue: minValue(1),
+                    integer
+                },
             }
         },
         methods: {
@@ -88,6 +155,45 @@
                     if (!this.$v.form.bonus_per_rubles.minValue) return "Значение должно быть ≥ 0";
                 }
             },
-        }
+            errorActivationBonusName() {
+                if (this.$v.form.activation_bonus_name.$dirty) {
+                    if (!this.$v.form.activation_bonus_name.required) return "Обязательное поле!";
+                }
+            },
+            errorActivationBonusValidPeriod() {
+                if (this.$v.form.activation_bonus_valid_period.$dirty) {
+                    if (!this.$v.form.activation_bonus_valid_period.required) return "Обязательное поле!";
+                    if (!this.$v.form.activation_bonus_valid_period.integer) return "Введите целое число!";
+                    if (!this.$v.form.activation_bonus_valid_period.minValue) return "Значение должно быть > 0";
+                }
+            },
+            errorActivationBonusValue() {
+                if (this.$v.form.activation_bonus_value.$dirty) {
+                    if (!this.$v.form.activation_bonus_value.required) return "Обязательное поле!";
+                    if (!this.$v.form.activation_bonus_value.integer) return "Введите целое число!";
+                   if (!this.$v.form.activation_bonus_value.minValue) return "Значение должно быть > 0";
+                }
+            },
+        },
+        watch: {
+            bonusActivationBtn() {
+                if (this.bonusActivationBtn) {
+                    this.updateInput('activation_bonus_name');
+                    this.updateInput('activation_bonus_valid_period');
+                    this.updateInput('activation_bonus_value');
+                } else {
+                    this.requestData.activation_bonus_name = null;
+                    this.requestData.activation_bonus_valid_period = null;
+                    this.requestData.activation_bonus_value = null;
+                }
+            },
+            bonusUnlimitedPeriodBtn() {
+                if (this.bonusUnlimitedPeriodBtn) {
+                    this.requestData.activation_bonus_valid_period = null;
+                } else {
+                    this.updateInput('activation_bonus_valid_period');
+                }
+            }
+        },
     }
 </script>
