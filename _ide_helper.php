bd73e771b03933f5e942eabe55785d12e3e8b7dc
ivan770 <leshenko.ivan770@gmail.com>
2020-06-26 18:40:58 +0300

    Обновлены зависимости

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.38 on 2020-06-17 19:19:37.
+ * Generated for Laravel 5.8.38 on 2020-06-24 18:09:47.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
