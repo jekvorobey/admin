c2e132356112d0d01f1a5ca8f275fefc2d51737d
Максим Купров <maxim.kyprov@ylab.io>
2021-06-29 11:19:05 +0300

    hotfix sentry identify user

--- config/app.php
+++ config/app.php
@@ -189,7 +189,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\SentryServiceProvider::class,
     ],
 
     /*
