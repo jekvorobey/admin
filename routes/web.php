0e0787c280a802743374a79a303e1137ec571e87
Stepan Prishepenko <itman116@gmail.com>
2021-09-23 17:02:52 +0300

    IBT-541: add web routes to activate/deactivate certificates

--- routes/web.php
+++ routes/web.php
@@ -221,6 +221,8 @@ Route::middleware('auth')->group(function () {
                 Route::prefix('{id}')->group(function () {
                     Route::get('', 'CertificateCardController@editPage')->name('certificate.card_edit');
                     Route::put('', 'CertificateCardController@update')->name('certificate.card_update');
+                    Route::put('activate', 'CertificateCardController@activate')->name('certificate.card_activate');
+                    Route::put('deactivate', 'CertificateCardController@deactivate')->name('certificate.card_deactivate');
                     Route::post('update/expired', 'CertificateCardController@updateExpireAt')->name('certificate.update_activation_period');
                     Route::post('notify', 'CertificateCardController@sendNotification')->name('certificate.send_notification');
                 });
