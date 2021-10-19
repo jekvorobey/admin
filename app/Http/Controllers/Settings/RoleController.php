6418994ed7c5fae573471aa49e1cf51f97a09516
Evgeniy Mokrov <e.mokrov@ylab.io>
2021-10-19 14:37:55 +0300

    IBT-441: RoleController fix

--- app/Http/Controllers/Settings/RoleController.php
+++ app/Http/Controllers/Settings/RoleController.php
@@ -13,8 +13,6 @@ use Greensight\CommonMsa\Rest\RestQuery;
 use Greensight\CommonMsa\Services\RoleService\RoleService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RoleController extends Controller
