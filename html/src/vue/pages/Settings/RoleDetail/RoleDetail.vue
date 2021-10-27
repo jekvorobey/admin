3ce190ef5b93841860040dd8179a0c5f8461889a
Evgeniy Mokrov <e.mokrov@ylab.io>
2021-10-27 16:37:56 +0300

    IBT-441: RoleDetail.vue fix

--- html/src/vue/pages/Settings/RoleDetail/RoleDetail.vue
+++ html/src/vue/pages/Settings/RoleDetail/RoleDetail.vue
@@ -1,135 +1,141 @@
 <template>
-    <layout-main>
-        <div class="d-flex justify-content-start align-items-start">
-            <div class="d-flex flex-column">
-                <shadow-card title="Основная информация" :buttons="{onEdit: 'pencil-alt'}" @onEdit="openModal('roleAdd')">
-                    <values-table :values="roleInfo" :names="roleValuesNames"/>
-                </shadow-card>
-                <shadow-card title="Блоки роли" padding="3" :buttons="{onAdd: 'plus'}" @onAdd="openModal('roleBlockAdd')">
-                    <table class="table table-sm">
-                        <thead>
-                        <tr>
-                            <th>Блок</th>
-                            <th>Разрешение</th>
-                            <th>Действия</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="blockPermission in selfBlockPermissions">
-                                <td>{{blockName(blockPermission.block_id)}}</td>
-                                <td>{{permissionName(blockPermission.permission_id)}}</td>
-                                <td><fa-icon @click="deleteBlock(blockPermission.id)" icon="trash-alt" class="icon-btn icon-btn--red"></fa-icon></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </shadow-card>
-            </div>
-        </div>
-        <role-edit-modal :source="role" :fronts="options.fronts" @onSave="updateRole"></role-edit-modal>
-        <add-role-block-modal :selfBlockPermissions.sync="selfBlockPermissions" :source="role" :allBlocks="options.blocks" :allPermissions="options.permissions" @onSave="onBlockCreated"></add-role-block-modal>
-    </layout-main>
+  <layout-main>
+    <div class="d-flex justify-content-start align-items-start">
+      <div class="d-flex flex-column">
+        <shadow-card title="Основная информация" :buttons="{onEdit: 'pencil-alt'}" @onEdit="openModal('roleAdd')">
+          <values-table :values="roleInfo" :names="roleValuesNames"/>
+        </shadow-card>
+        <shadow-card title="Блоки роли" padding="3" :buttons="{onAdd: 'plus'}" @onAdd="openModal('roleBlockAdd')">
+          <table class="table table-sm">
+            <thead>
+            <tr>
+              <th>Блок</th>
+              <th>Разрешение</th>
+              <th>Действия</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="blockPermission in selfBlockPermissions">
+              <td>{{ blockName(blockPermission.block_id) }}</td>
+              <td>{{ permissionName(blockPermission.permission_id) }}</td>
+              <td>
+                <fa-icon @click="deleteBlock(blockPermission.id)" icon="trash-alt"
+                         class="icon-btn icon-btn--red"></fa-icon>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </shadow-card>
+      </div>
+    </div>
+    <role-edit-modal :source="role" :fronts="options.fronts" @onSave="updateRole"></role-edit-modal>
+    <add-role-block-modal :selfBlockPermissions.sync="selfBlockPermissions" :source="role" :allBlocks="options.blocks"
+                          :allPermissions="options.permissions" @onSave="onBlockCreated"></add-role-block-modal>
+  </layout-main>
 </template>
 
 <script>
 
-    import Services from '../../../../scripts/services/services';
-    import modalMixin from '../../../mixins/modal.js';
+import Services from '../../../../scripts/services/services';
+import modalMixin from '../../../mixins/modal.js';
 
-    import ShadowCard from '../../../components/shadow-card.vue';
-    import ValuesTable from '../../../components/values-table.vue';
+import ShadowCard from '../../../components/shadow-card.vue';
+import ValuesTable from '../../../components/values-table.vue';
 
-    import modal from '../../../components/controls/modal/modal.vue';
-    import VSelect from '../../../components/controls/VSelect/VSelect.vue';
+import modal from '../../../components/controls/modal/modal.vue';
+import VSelect from '../../../components/controls/VSelect/VSelect.vue';
 
-    import RoleEditModal from '../components/role-add-modal.vue';
-    import AddRoleBlockModal from '../components/add-role-block-modal.vue';
+import RoleEditModal from '../components/role-add-modal.vue';
+import AddRoleBlockModal from '../components/add-role-block-modal.vue';
 
-    export default {
-        mixins: [modalMixin],
-        components: {
-            ShadowCard,
-            ValuesTable,
-            modal,
-            VSelect,
-            RoleEditModal,
-            AddRoleBlockModal
-        },
-        props: {
-            iRole: {},
-            iBlockPermissions: {},
-            options: {},
-        },
-        data() {
-            return {
-                role: this.iRole,
-                selfBlockPermissions: this.iBlockPermissions,
-                roleValuesNames: {
-                    name: 'Наименование',
-                    front: 'Система',
-                    created_at: 'Дата добавления',
-                    updated_at: 'Дата обновления',
-                },
-                newBlock: ''
-            };
-        },
-        methods: {
-            frontName(id) {
-                let fronts = Object.values(this.options.fronts).filter(front => front.id === id);
-                return fronts.length > 0 ? fronts[0].name : 'N/A';
-            },
-            blockName(id) {
-                let allBlocks = Object.values(this.options.blocks).filter(block => block.id === id);
-                return allBlocks.length > 0 ? allBlocks[0].name : 'N/A';
-            },
-            permissionName(id) {
-                let permissions = Object.values(this.options.permissions).filter(permission => permission.id === id);
-                return permissions.length > 0 ? permissions[0].name : 'N/A';
-            },
-            deleteBlock(id) {
-                Services.net().post(this.getRoute('settings.deleteBlock', {blockId: id}), {}, {
-                  role_id: this.role.id
-                },{})
-                    .then(data => {
-                        this.selfBlockPermissions = data.blockPermissions;
-                        this.showMessageBox({text: 'Блок удален'});
-                    });
-            },
-            updateRole(newData) {
-                Object.assign(this.role, newData.role);
-                this.closeModal();
-                this.showMessageBox({text: 'Роль обновлена'});
-            },
-            onBlockCreated(newData) {
-              this.selfBlockPermissions = newData;
-              this.closeModal();
-              this.showMessageBox({text: 'Блок добавлен'});
-            }
-        },
-        computed: {
-            roleInfo() {
-                return {
-                    name: this.role.name,
-                    front: this.frontName(this.role.front),
-                    created_at: this.datePrint(this.role.created_at),
-                    updated_at: this.datePrint(this.role.updated_at)
-                };
-            },
-            blockOptions() {
-                return this.options.blocks.map(block => ({value: block.id, text: block.name}))
-            }
-        }
+export default {
+  mixins: [modalMixin],
+  components: {
+    ShadowCard,
+    ValuesTable,
+    modal,
+    VSelect,
+    RoleEditModal,
+    AddRoleBlockModal
+  },
+  props: {
+    iRole: {},
+    iBlockPermissions: {},
+    options: {},
+  },
+  data() {
+    return {
+      role: this.iRole,
+      selfBlockPermissions: this.iBlockPermissions,
+      roleValuesNames: {
+        name: 'Наименование',
+        front: 'Система',
+        created_at: 'Дата добавления',
+        updated_at: 'Дата обновления',
+      },
+      newBlock: ''
     };
+  },
+  methods: {
+    frontName(id) {
+      let fronts = Object.values(this.options.fronts).filter(front => front.id === id);
+      return fronts.length > 0 ? fronts[0].name : 'N/A';
+    },
+    blockName(id) {
+      let allBlocks = Object.values(this.options.blocks).filter(block => block.id === id);
+      return allBlocks.length > 0 ? allBlocks[0].name : 'N/A';
+    },
+    permissionName(id) {
+      let permissions = Object.values(this.options.permissions).filter(permission => permission.id === id);
+      return permissions.length > 0 ? permissions[0].name : 'N/A';
+    },
+    deleteBlock(id) {
+      Services.net().post(this.getRoute('settings.deleteBlock', {blockId: id}), {}, {
+        role_id: this.role.id
+      }, {})
+          .then(data => {
+            this.selfBlockPermissions = data.blockPermissions;
+            this.showMessageBox({text: 'Блок удален'});
+          });
+    },
+    updateRole(newData) {
+      Object.assign(this.role, newData.role);
+      this.closeModal();
+      this.showMessageBox({text: 'Роль обновлена'});
+    },
+    onBlockCreated(newData) {
+      this.selfBlockPermissions = newData;
+      this.closeModal();
+      this.showMessageBox({text: 'Блок добавлен'});
+    }
+  },
+  computed: {
+    roleInfo() {
+      return {
+        name: this.role.name,
+        front: this.frontName(this.role.front),
+        created_at: this.datePrint(this.role.created_at),
+        updated_at: this.datePrint(this.role.updated_at)
+      };
+    },
+    blockOptions() {
+      return this.options.blocks.map(block => ({value: block.id, text: block.name}))
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .icon-btn {
-        cursor: pointer;
-        transition: 0.3s all;
-    }
-    .icon-btn--red {
-        color:var(--red);
-    }
-    .icon-btn--red:hover {
-        color:var(--pink);
-    }
+.icon-btn {
+  cursor: pointer;
+  transition: 0.3s all;
+}
+
+.icon-btn--red {
+  color: var(--red);
+}
+
+.icon-btn--red:hover {
+  color: var(--pink);
+}
 </style>
