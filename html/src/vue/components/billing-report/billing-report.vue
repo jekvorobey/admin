dcd0431fd4ddb0cd98744a329dd71997c233373b
Vladimir Admaev <vladimir.admaev@ylab.io>
2021-11-15 18:26:56 +0400

    IBT-495 Add vue component for billing reports. Replace billing from tab-billing to new component. Delete extra methods and params. Check working capacity

---
+++ html/src/vue/components/billing-report/billing-report.vue
@@ -0,0 +1,257 @@
+<template>
+    <div>
+        <h4>Биллинговый период</h4>
+        <span class="custom-control custom-switch">
+        <input type="checkbox" class="custom-control-input" id="monthly-merchant_cycle" key="monthly" v-model="monthly">
+        <label class="custom-control-label" for="monthly-merchant_cycle"></label>
+        <label for="monthly-merchant_cycle">Календарный месяц</label>
+    </span>
+        <div v-if="!monthly">
+            <div class="row">
+                <v-input
+                    v-model="form.billing_cycle"
+                    class="col-4"
+                    type="number"
+                    min="1"
+                    step="1"
+                    help="период указывается в днях">Произвольный период
+                </v-input>
+
+                <div class="col-12" v-if="canUpdate(blocks.merchants)">
+                    <button class="btn btn-sm btn-success" :disabled="form.billing_cycle <= 0" @click="saveBillingCycle">
+                        Сохранить
+                    </button>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+        <h4>Отчеты</h4>
+        <table>
+            <tr>
+                <td>
+                    <date-picker
+                        v-model="billing.period"
+                        value-type="format"
+                        format="YYYY-MM-DD"
+                        range
+                        input-class="form-control form-control-sm"
+                    />
+                </td>
+                <td>
+                    <button
+                        class="btn btn-success btn-sm"
+                        :disabled="!isPeriod(billing.period)"
+                        @click="createReport">Сделать внеочередной биллинг
+                    </button>
+                </td>
+            </tr>
+        </table>
+
+        <hr>
+        <h4 class="mt-4">Отчеты комиссионера</h4>
+        <table class="table mt-2">
+            <tr>
+                <td>Период</td>
+                <td>Статус</td>
+                <td>К выплате, р</td>
+                <td>Документ</td>
+                <td>Создан</td>
+                <td>Изменен</td>
+                <td v-if="canUpdate(blocks.merchants)">Действия</td>
+            </tr>
+            <tr v-for="report in billingReports">
+                <td>{{ report.date_from }} &ndash; {{ report.date_to }}</td>
+                <td>
+                    <b-badge :variant="getBadge(report.status)">
+                        {{ getStatus(report.status) }}
+                    </b-badge>
+                </td>
+                <td>{{ report.sum.toLocaleString() }}</td>
+                <td>
+                    <a target="_blank" :href="$store.getters.getRoute('merchant.detail.billingReport.download',
+          {id:getMerchantId, reportId:report.id})">Скачать</a>
+                </td>
+                <td>{{ report.created_at }}</td>
+                <td>{{ report.updated_at }}</td>
+                <td v-if="canUpdate(blocks.merchants)">
+                    <b-button v-if="report.status === 0" class="btn btn-warning btn-sm" @click="updateStatus(report.id, 4)">
+                        Отправить
+                        <fa-icon icon="check"/>
+                    </b-button>
+                    <b-button v-else-if="report.status !== 2 && report.status !== 0" class="btn btn-success btn-sm"
+                              @click="updateStatus(report.id, 2)">
+                        Подтвердить
+                        <fa-icon icon="check"/>
+                    </b-button>
+                    <b-button class="btn btn-danger btn-sm" @click="removeItem(report.id)">
+                        Удалить
+                        <fa-icon icon="trash-alt"/>
+                    </b-button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import Services from "../../../scripts/services/services";
+import VInput from "../controls/VInput/VInput.vue";
+import DatePicker from "vue2-datepicker";
+import {mapActions} from "vuex";
+
+export default {
+    name: 'billing-report',
+    props: ['model'],
+    components: {
+        VInput,
+        DatePicker
+    },
+    data() {
+        return {
+            form: {
+                billing_cycle: null,
+            },
+            monthly: true,
+            billing: {
+                period: null,
+            },
+            billingList: {},
+            billingReports: {},
+            statuses: [
+                {text:'модерация', badge:'light'},
+                {text:'просмотрен', badge:'primary'},
+                {text:'подтвержден', badge:'success'},
+                {text:'отклонен', badge:'danger'},
+                {text:'отправлен', badge:'warning'},
+                {text:'оплачен', badge:'success'},
+            ],
+            newReportDates: {
+                dateFrom:null,
+                dateTo:null,
+            },
+            billingOperation: {},
+        }
+    },
+    methods: {
+        ...mapActions({
+            showMessageBox: 'modal/showMessageBox',
+        }),
+        setMonthlyPeriod() {
+            if (this.monthly && this.form.billing_cycle && this.form.billing_cycle !== 0) {
+
+                this.form.billing_cycle = 0;
+                this.saveBillingCycle();
+            }
+        },
+        saveBillingCycle() {
+            Services.showLoader();
+            Services.net().put(this.getRoute('merchant.detail.billing.billing_cycle',
+                    {id: this.model.id}),
+                {
+                    billing_cycle: this.form.billing_cycle
+                }).then(data => {
+                if (!data) {
+                    Services.msg('Изменения успешно сохранены')
+                }
+            }, () => {
+                Services.msg('Не удалось сохранить изменения', 'danger')
+            }).finally(() => {
+                Services.hideLoader();
+            })
+        },
+        isPeriod(period) {
+            if (!period || period.length !== 2) {
+                return false;
+            }
+            this.newReportDates = {
+                dateFrom: period[0],
+                dateTo: period[1],
+            }
+            return period[0] && period[1];
+        },
+        createReport() {
+            if (!this.newReportDates) { return false; }
+            Services.showLoader();
+            Services.net()
+                .post(this.getRoute('merchant.detail.billingReport.create', {id: this.model.id,}), {},
+                    { date_from: this.newReportDates.dateFrom,  date_to: this.newReportDates.dateTo})
+                .then(() => {
+                    this.loadReports();
+                    this.newReportDates = {dateFrom: null, dateTo: null};
+                    this.billing.period = null;
+                })
+                .catch(() => {
+                    this.showMessageBox({title: 'Ошибка', text: 'Попробуйте позже'});
+                }).finally(() => {
+                Services.hideLoader();
+                this.showMessageBox({title: 'Отчет создан'});
+            });
+        },
+        removeItem(id) {
+            Services.showLoader();
+            Services.net()
+                .delete(this.getRoute('merchant.detail.billingReport.delete', {id: this.model.id, reportId: id,}))
+                .then(() => {
+                    this.loadReports();
+                })
+                .catch(() => {
+                    this.showMessageBox({title: 'Ошибка', text: 'Попробуйте позже'});
+                }).finally(() => {
+                Services.hideLoader();
+                this.showMessageBox({title: 'Отчет удалён'});
+            });
+        },
+        updateStatus(id, status) {
+            Services.showLoader();
+            Services.net()
+                .put(this.getRoute('merchant.detail.billingReport.updateStatus', {id: this.model.id, reportId: id,}), {},{status: status})
+                .then(() => {
+                    this.loadReports();
+                })
+                .catch(() => {
+                    this.showMessageBox({title: 'Ошибка', text: 'Попробуйте позже'});
+                }).finally(() => {
+                Services.hideLoader();
+                this.showMessageBox({title: 'Статус Обновлен'});
+            });
+        },
+        loadReports() {
+            Services.showLoader();
+            Services.net().get(this.getRoute('merchant.detail.billingReport', {id: this.model.id}))
+                .then(data => {
+                    this.billingReports = data.billing_reports;
+                }).finally(() => {
+                Services.hideLoader();
+            });
+        },
+        getMerchantId() {
+            return this.model.id;
+        },
+        getStatus(id) {
+            return this.statuses[id].text;
+        },
+        getBadge(id) {
+            return this.statuses[id].badge;
+        },
+    },
+    created() {
+        Services.net().get(this.getRoute('merchant.detail.billing', {id: this.model.id}))
+            .then(data => {
+                if (data.billing_cycle) {
+                    this.form.billing_cycle = data.billing_cycle;
+                    this.monthly = false;
+                }
+            })
+            .finally(() => {
+                Services.hideLoader();
+            });
+        this.loadReports();
+    },
+    watch: {
+        monthly: function () {
+            this.setMonthlyPeriod();
+        }
+    }
+}
+</script>
