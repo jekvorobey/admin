5c4672d7a683e043d2c68bff3ff499badd2ed272
Evgeniy Mokrov <e.mokrov@ylab.io>
2021-11-18 08:48:49 +0300

    IBT-437: fix specialty list

--- app/Http/Controllers/PublicEvent/SpecialtyController.php
+++ app/Http/Controllers/PublicEvent/SpecialtyController.php
@@ -5,68 +5,79 @@ namespace App\Http\Controllers\PublicEvent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SpecialtyRequest;
 use Greensight\CommonMsa\Dto\BlockDto;
+use Greensight\CommonMsa\Rest\RestQuery;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Pim\Core\PimException;
 use Pim\Dto\PublicEvent\SpecialtyDto;
 use Pim\Services\PublicEventSpecialtyService\PublicEventSpecialtyService;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class SpecialtyController extends Controller
 {
+    /**
+     * @throws PimException
+     */
     public function list(Request $request, PublicEventSpecialtyService $publicEventSpecialtyService)
     {
         $this->canView(BlockDto::ADMIN_BLOCK_PUBLIC_EVENTS);
 
         $this->title = 'Направления';
 
-        $page = $request->get('page', 1);
-        [$total, $specialties] = $this->loadSpecialties($publicEventSpecialtyService, $page);
+        $query = $this->makeQuery($request);
 
         return $this->render('PublicEvent/SpecialtyList', [
-            'iSpecialties' => $specialties,
-            'iTotal' => $total['total'],
-            'iCurrentPage' => $page,
+            'iSpecialties' => $this->loadItems($query, $publicEventSpecialtyService),
+            'iPager' => $publicEventSpecialtyService->count($query),
+            'iCurrentPage' => $request->get('page', 1),
         ]);
     }
 
+    /**
+     * @throws PimException
+     */
     public function page(Request $request, PublicEventSpecialtyService $publicEventSpecialtyService): JsonResponse
     {
         $this->canView(BlockDto::ADMIN_BLOCK_PUBLIC_EVENTS);
 
-        $page = $request->get('page', 1);
-        [$total, $specialties] = $this->loadSpecialties($publicEventSpecialtyService, $page);
+        $query = $this->makeQuery($request);
+        $data = [
+            'items' => $this->loadItems($query, $publicEventSpecialtyService),
+        ];
+        if ($request->get('page', 1) == 1) {
+            $data['pager'] = $publicEventSpecialtyService->count($query);
+        }
 
-        return response()->json([
-            'specialties' => $specialties,
-            'total' => $total['total'],
-        ]);
+        return response()->json($data);
     }
 
+    /**
+     * @throws PimException
+     */
     public function save(
         SpecialtyRequest $request,
         PublicEventSpecialtyService $publicEventSpecialtyService
     ): JsonResponse {
         $this->canUpdate(BlockDto::ADMIN_BLOCK_PUBLIC_EVENTS);
-
         $data = $request->all();
-
         $specialty = new SpecialtyDto($data);
-
         if (isset($data['id'])) {
             $publicEventSpecialtyService->update($data['id'], $specialty);
         } else {
             $publicEventSpecialtyService->create($specialty);
         }
-        $page = $request->get('page', 1);
-        [$total, $specialties] = $this->loadSpecialties($publicEventSpecialtyService, $page);
+        $query = $this->makeQuery($request);
 
         return response()->json([
-            'specialties' => $specialties,
-            'iTotal' => $total['total'],
-            'iCurrentPage' => $page,
+            'specialty' => $specialty,
+            'specialties' => $this->loadItems($query, $publicEventSpecialtyService),
+            'iPager' => $publicEventSpecialtyService->count($query),
         ]);
     }
 
+    /**
+     * @throws PimException
+     */
     public function delete(Request $request, PublicEventSpecialtyService $publicEventSpecialtyService): JsonResponse
     {
         $this->canUpdate(BlockDto::ADMIN_BLOCK_PUBLIC_EVENTS);
@@ -74,27 +85,30 @@ class SpecialtyController extends Controller
         $id = $request->get('id');
 
         if (!$id) {
-            throw new BadRequestHttpException('ids required');
+            throw new BadRequestHttpException('id required');
         }
         $publicEventSpecialtyService->delete($id);
-
-        $page = $request->get('page', 1);
-        [$total, $specialties] = $this->loadSpecialties($publicEventSpecialtyService, $page);
+        $query = $this->makeQuery($request);
 
         return response()->json([
-            'specialties' => $specialties,
-            'iTotal' => $total['total'],
-            'iCurrentPage' => $page,
+            'specialties' => $this->loadItems($query, $publicEventSpecialtyService),
+            'iPager' => $publicEventSpecialtyService->count($query),
         ]);
     }
 
-    private function loadSpecialties(PublicEventSpecialtyService $publicEventSpecialtyService, $page): array
+    protected function makeQuery(Request $request): RestQuery
     {
-        $query = $publicEventSpecialtyService->query()->pageNumber($page, 20);
+        $restQuery = new RestQuery();
+        $restQuery->pageNumber($request->get('page', 1), 20);
 
-        $total = $publicEventSpecialtyService->count($query);
-        $specialties = $publicEventSpecialtyService->find($query);
+        return $restQuery;
+    }
 
-        return [$total, $specialties];
+    /**
+     * @throws PimException
+     */
+    protected function loadItems(RestQuery $query, PublicEventSpecialtyService $publicEventSpecialtyService)
+    {
+        return $publicEventSpecialtyService->specialties($query);
     }
 }
--- html/src/vue/pages/PublicEvent/SpecialtyList/SpecialtyList.vue
+++ html/src/vue/pages/PublicEvent/SpecialtyList/SpecialtyList.vue
@@ -2,32 +2,49 @@
     <layout-main>
         <b-row class="mb-2" v-if="canUpdate(blocks.events)">
             <b-col>
-                <button class="btn btn-success btn-sm" @click="openSpecialty()"><fa-icon icon="plus"/></button>
+                <button class="btn btn-success btn-sm" @click="openSpecialty()">
+                    <fa-icon icon="plus"/>
+                </button>
             </b-col>
         </b-row>
         <table class="table">
             <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Название</th>
-                    <th>Название на фронте</th>
-                    <th>Активность</th>
-                    <th v-if="canUpdate(blocks.events)"></th>
-                </tr>
+            <tr>
+                <th>ID</th>
+                <th>Название</th>
+                <th>Название на фронте</th>
+                <th>Активность</th>
+                <th v-if="canUpdate(blocks.events)"></th>
+            </tr>
             </thead>
             <tbody>
-                <tr v-for="specialty in specialties">
-                    <td>{{ specialty.id }}</td>
-                    <td>{{ specialty.name }}</td>
-                    <td>{{ specialty.display_name }}</td>
-                    <td>{{ specialty.active ? 'Да' : 'Нет' }}</td>
-                    <td v-if="canUpdate(blocks.events)">
-                        <button class="btn btn-warning btn-sm" @click="openSpecialty(specialty)"><fa-icon icon="edit"/></button>
-                        <button class="btn btn-danger btn-sm" @click="deleteSpecialty(specialty.id)"><fa-icon icon="delete"/></button>
-                    </td>
-                </tr>
+            <tr v-for="specialty in specialties">
+                <td>{{ specialty.id }}</td>
+                <td>{{ specialty.name }}</td>
+                <td>{{ specialty.display_name }}</td>
+                <td>{{ specialty.active ? 'Да' : 'Нет' }}</td>
+                <td v-if="canUpdate(blocks.events)">
+                    <button class="btn btn-warning btn-sm" @click="openSpecialty(specialty)">
+                        <fa-icon icon="edit"/>
+                    </button>
+                    <button class="btn btn-danger btn-sm" @click="deleteSpecialty(specialty.id)">
+                        <fa-icon icon="trash-alt"/>
+                    </button>
+                </td>
+            </tr>
             </tbody>
         </table>
+        <div>
+            <b-pagination
+                v-if="pager.pages > 1"
+                v-model="currentPage"
+                :total-rows="pager.total"
+                :per-page="pager.pageSize"
+                @change="changePage"
+                :hide-goto-end-buttons="pager.pages < 20"
+                class="mt-3 float-right"
+            ></b-pagination>
+        </div>
         <form-modal modal-name="FormSpecialty" @accept="saveSpecialty" :model.sync="specialty"/>
     </layout-main>
 </template>
@@ -37,23 +54,47 @@
 import FormModal from './components/form-modal.vue';
 import modalMixin from '../../../mixins/modal';
 import Services from '../../../../scripts/services/services.js';
+import withQuery from "with-query";
 
 export default {
     mixins: [modalMixin],
-    props: ['iSpecialties'],
+    props: {
+        iSpecialties: {},
+        iPager: {},
+        iCurrentPage: {},
+    },
     components: {FormModal},
     data() {
         return {
             specialties: this.iSpecialties,
-            specialty: {}
+            specialty: {},
+            pager: this.iPager,
+            currentPage: this.iCurrentPage,
         };
     },
     methods: {
+        changePage(newPage) {
+            history.pushState(null, null, location.origin + location.pathname + withQuery('', {
+                page: newPage,
+            }));
+        },
+        loadPage() {
+            Services.showLoader();
+            Services.net().get(this.route('public-event.specialties.page'), {
+                page: this.currentPage,
+            }).then(data => {
+                this.specialties = data.items;
+                if (data.pager) {
+                    this.pager = data.pager
+                }
+            }).finally(() => {
+                Services.hideLoader();
+            });
+        },
         fillSpecialty(specialty) {
             return {
                 id: specialty ? specialty.id : null,
                 name: specialty ? specialty.name : '',
-                display_name: specialty ? specialty.display_name : '',
                 active: specialty ? specialty.active : 1,
             }
         },
@@ -62,7 +103,7 @@ export default {
             this.openModal('FormSpecialty');
         },
         saveSpecialty() {
-            Services.net().post(this.getRoute('public-event.specialties.save'), {}, this.specialty).then((data)=> {
+            Services.net().post(this.getRoute('public-event.specialties.save'), {}, this.specialty).then((data) => {
                 this.specialties = data.specialties;
                 this.closeModal('FormSpecialty');
                 this.$bvToast.toast(`Направление ${this.specialty.name} сохранено`, {
@@ -73,12 +114,17 @@ export default {
             });
         },
         deleteSpecialty(id) {
-            Services.net().delete(this.getRoute('public-event.specialties.delete', {id: id}), {}, {}, {})
+            Services.net().post(this.getRoute('public-event.specialties.delete'), {}, {id: id}, {})
                 .then(data => {
                     this.specialties = data.specialties;
                     this.showMessageBox({text: 'Направление удалено'});
                 });
         },
+    },
+    watch: {
+        currentPage() {
+            this.loadPage();
+        }
     },
 };
 </script>
