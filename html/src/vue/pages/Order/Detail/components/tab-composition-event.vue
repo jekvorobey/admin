e30e6e79a4108487f394da0fb0ca27218d306ae9
Evgeniy Mokrov <e.mokrov@ylab.io>
2021-11-23 19:00:10 +0300

    IBT-437: fix order event composition

--- html/src/vue/pages/Order/Detail/components/tab-composition-event.vue
+++ html/src/vue/pages/Order/Detail/components/tab-composition-event.vue
@@ -1,31 +1,39 @@
 <template>
     <table class="table">
         <thead>
-        <tr>
-            <th>Тип билета</th>
-            <th>МК</th>
-            <th>Количество</th>
-            <th>Стоимость</th>
-            <th>Участники</th>
-        </tr>
         </thead>
         <tbody>
         <tr v-for="event in orderInfo.publicEvents">
-            <td>{{ event.ticketsInfo.ticket_type_name }}</td>
-            <td v-if="canView(blocks.events)">
-                <a :href="getRoute('public-event.detail', { event_id: event.id })" target="_blank">
-                    {{ event.ticketsInfo.name }}
-                </a>
-            </td>
-            <td v-else>{{ event.ticketsInfo.name }}</td>
-            <td>{{ preparePrice(event.ticketsInfo.tickets_qty) }} шт.</td>
-            <td>{{ preparePrice(event.ticketsInfo.price) }} руб.</td>
-            <td>
-                <ul>
-                    <li v-for="ticket in event.ticketsInfo.tickets">
-                        {{ ticket.first_name }} {{ ticket.middle_name }} {{ ticket.last_name }}
-                    </li>
-                </ul>
+            <td class="inner">
+                <table class="table">
+                    <tr>
+                        <th>МК</th>
+                        <th>Тип билета</th>
+                        <th>Количество</th>
+                        <th>Стоимость</th>
+                        <th>Участники</th>
+                    </tr>
+                    <tr v-for="info in event.ticketsInfo">
+                        <td v-if="canView(blocks.events)">
+                            <a :href="getRoute('public-event.detail', { event_id: event.id })" target="_blank">
+                                {{ info.name }}
+                            </a>
+                        </td>
+                        <td v-else>{{ info.name }}</td>
+                        <td>{{ info.ticket_type_name }}</td>
+                        <td>{{ info.tickets_qty }} шт.</td>
+                        <td>{{ preparePrice(info.price) }} руб.</td>
+                        <td class="inner">
+                            <table class="table">
+                                <tr v-for="ticket in info.tickets">
+                                    <td>{{ ticket.first_name }} {{ ticket.middle_name }} {{ ticket.last_name }}</td>
+                                    <td>{{ ticket.phone }}</td>
+                                    <td>{{ ticket.email }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
             </td>
         </tr>
         </tbody>
@@ -33,27 +41,28 @@
 </template>
 
 <script>
-    import Services from "../../../../../scripts/services/services";
+import Services from "../../../../../scripts/services/services";
 
-    export default {
-        name: "tab-composition-event",
-        props: [
-            'model',
-        ],
-        methods: {
-            openTab(tab) {
-                Services.event().$emit('showTab', tab);
-            },
+export default {
+    name: "tab-composition-event",
+    props: [
+        'model',
+
+    ],
+    methods: {
+        openTab(tab) {
+            Services.event().$emit('showTab', tab);
         },
-        computed: {
-            orderInfo: {
-                get() {
-                    return this.model
-                },
-                set(value) {
-                    this.$emit('update:model', value)
-                },
-            }
+    },
+    computed: {
+        orderInfo: {
+            get() {
+                return this.model
+            },
+            set(value) {
+                this.$emit('update:model', value)
+            },
         }
     }
+}
 </script>
