cc27c69a5826a17b98a7b23bc963950537a91642
Максим Купров <maxim.kyprov@ylab.io>
2022-04-12 15:39:42 +0300

    some fixes on integrations tab

--- app/Http/Controllers/Merchant/Detail/TabExtSystemsController.php
+++ app/Http/Controllers/Merchant/Detail/TabExtSystemsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\MerchantIntegration\IntegrationRequest;
 use Greensight\CommonMsa\Dto\BlockDto;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use MerchantManagement\Dto\Integration\ExtSystemDriver;
 use MerchantManagement\Dto\Integration\ExtSystemDto;
@@ -19,17 +20,23 @@ class TabExtSystemsController extends Controller
     protected const MERCHANT_SETTING_PRICE_NAME = 'moy_sklad_import_price_type_name';
     protected const MERCHANT_SETTING_ORGANIZATION_NAME = 'moy_sklad_order_organization';
 
-    public function load(
-        int $merchantId,
-        MerchantService $merchantService,
-        MerchantIntegrationService $merchantIntegrationService
-    ): JsonResponse {
+    private MerchantIntegrationService $merchantIntegrationService;
+    private MerchantService $merchantService;
+
+    public function __construct(MerchantIntegrationService $merchantIntegrationService, MerchantService $merchantService)
+    {
+        $this->merchantIntegrationService = $merchantIntegrationService;
+        $this->merchantService = $merchantService;
+    }
+
+    public function load(int $merchantId): JsonResponse
+    {
         $this->canView(BlockDto::ADMIN_BLOCK_MERCHANTS);
 
-        $restQuery = $merchantIntegrationService->newQuery()->setFilter('merchant_id', $merchantId);
-        $extSystem = $merchantIntegrationService->extSystems($restQuery)->first();
-        $merchantPriceSetting = $merchantService->getSetting($merchantId, self::MERCHANT_SETTING_PRICE_NAME)->first();
-        $merchantOrganizationSetting = $merchantService->getSetting($merchantId, self::MERCHANT_SETTING_ORGANIZATION_NAME)->first();
+        $restQuery = $this->merchantIntegrationService->newQuery()->setFilter('merchant_id', $merchantId);
+        $extSystem = $this->merchantIntegrationService->extSystems($restQuery)->first();
+        $merchantPriceSetting = $this->merchantService->getSetting($merchantId, self::MERCHANT_SETTING_PRICE_NAME)->first();
+        $merchantOrganizationSetting = $this->merchantService->getSetting($merchantId, self::MERCHANT_SETTING_ORGANIZATION_NAME)->first();
         $extSystemsOptions = [
             ExtSystemDriver::driverById(ExtSystemDriver::DRIVER_1C),
             ExtSystemDriver::driverById(ExtSystemDriver::DRIVER_MOY_SKLAD),
@@ -41,7 +48,7 @@ class TabExtSystemsController extends Controller
         $paramOrder = null;
         $paramPriceStock = null;
         if ($extSystem) {
-            $integration = $merchantIntegrationService->integrations($extSystem->id);
+            $integration = $this->merchantIntegrationService->integrations($extSystem->id);
             $driver = (int) $extSystem->driver;
             switch ($driver) {
                 case ExtSystemDriver::DRIVER_1C:
@@ -75,17 +82,13 @@ class TabExtSystemsController extends Controller
         ]);
     }
 
-    public function create(
-        int $merchantId,
-        IntegrationRequest $request,
-        MerchantService $merchantService,
-        MerchantIntegrationService $merchantIntegrationService
-    ): JsonResponse {
+    public function create(int $merchantId, IntegrationRequest $request): JsonResponse
+    {
         $this->canUpdate(BlockDto::ADMIN_BLOCK_MERCHANTS);
 
         $data = $request->all();
 
-        $merchant = $merchantService->merchant($merchantId);
+        $merchant = $this->merchantService->merchant($merchantId);
         $connectionParams = [];
         switch ($data['driver']) {
             case ExtSystemDriver::DRIVER_1C:
@@ -129,21 +132,9 @@ class TabExtSystemsController extends Controller
             'connection_params' => $connectionParams,
         ]);
 
-        $extSystemId = $merchantIntegrationService->createExtSystem($extSystem);
-        if ($data['driver'] === ExtSystemDriver::DRIVER_MOY_SKLAD) {
-            foreach ($this->moyskladIntegrationsData($data['integrationParams']) as $integrationData) {
-                $integrationDto = new IntegrationDto($integrationData);
-                $merchantIntegrationService->createIntegration($extSystemId, $integrationDto);
-            }
-            $merchantService->setSetting($merchantId, self::MERCHANT_SETTING_PRICE_NAME, $data['settingPriceValue']);
-            $merchantService->setSetting($merchantId, self::MERCHANT_SETTING_ORGANIZATION_NAME, $data['settingOrganizationValue']);
-        }
-        if ($data['driver'] === ExtSystemDriver::DRIVER_FILE_SHARING) {
-            foreach ($this->fileSharingIntegrationsData($data['integrationParams']) as $integrationData) {
-                $integrationDto = new IntegrationDto($integrationData);
-                $merchantIntegrationService->createIntegration($extSystemId, $integrationDto);
-            }
-        }
+        $extSystemId = $this->merchantIntegrationService->createExtSystem($extSystem);
+        $this->saveIntegrations($data, collect(), $extSystemId);
+        $this->saveSettings($data);
 
         return response()->json([]);
     }
@@ -151,7 +142,6 @@ class TabExtSystemsController extends Controller
     public function update(
         int $extSystemId,
         IntegrationRequest $request,
-        MerchantService $merchantService,
         MerchantIntegrationService $merchantIntegrationService
     ): JsonResponse {
         $this->canUpdate(BlockDto::ADMIN_BLOCK_MERCHANTS);
@@ -177,30 +167,55 @@ class TabExtSystemsController extends Controller
         }
         $extSystem = new ExtSystemDto([
             'merchant_id' => $data['merchantId'],
-            'connection_params' => $connectionParams,
+            'connection_params' => $connectionParams ?? [],
         ]);
 
         $merchantIntegrationService->updateExtSystem($extSystemId, $extSystem);
-        if ((int) $data['driver'] === ExtSystemDriver::DRIVER_MOY_SKLAD) {
-            $merchantService->setSetting($data['merchantId'], self::MERCHANT_SETTING_PRICE_NAME, $data['settingPriceValue']);
-            $merchantService->setSetting($data['merchantId'], self::MERCHANT_SETTING_ORGANIZATION_NAME, $data['settingOrganizationValue']);
-            $integrations = $merchantIntegrationService->integrations($extSystemId);
-            foreach ($this->moyskladIntegrationsData($data['integrationParams']) as $integrationData) {
-                $integrationDto = new IntegrationDto($integrationData);
-                $integration = $integrations->where('type', $integrationData['type'])->first();
-                $merchantIntegrationService->updateIntegration($integration->id, $integrationDto);
-            }
-        }
-        if ((int) $data['driver'] === ExtSystemDriver::DRIVER_FILE_SHARING) {
-            $integrations = $merchantIntegrationService->integrations($extSystemId);
-            foreach ($this->fileSharingIntegrationsData($data['integrationParams']) as $integrationData) {
-                $integrationDto = new IntegrationDto($integrationData);
-                $integration = $integrations->where('type', $integrationData['type'])->first();
-                $merchantIntegrationService->updateIntegration($integration->id, $integrationDto);
-            }
-        }
+
+        $integrations = $merchantIntegrationService->integrations($extSystemId);
+        $this->saveIntegrations($data, $integrations, $extSystemId);
+        $this->saveSettings($data);
 
         return response()->json([]);
+    }
+
+    private function saveIntegrations(array $data, Collection $integrations, int $extSystemId): void
+    {
+        switch ((int) $data['driver']) {
+            case ExtSystemDriver::DRIVER_MOY_SKLAD:
+                foreach ($this->moyskladIntegrationsData($data['integrationParams']) as $integrationData) {
+                    $integrationDto = new IntegrationDto($integrationData);
+
+                    $integration = $integrations->firstWhere('type', $integrationData['type']);
+                    if ($integration) {
+                        $this->merchantIntegrationService->updateIntegration($integration->id, $integrationDto);
+                    } else {
+                        $this->merchantIntegrationService->createIntegration($extSystemId, $integrationDto);
+                    }
+                }
+                return;
+            case ExtSystemDriver::DRIVER_FILE_SHARING:
+                foreach ($this->fileSharingIntegrationsData($data['integrationParams']) as $integrationData) {
+                    $integrationDto = new IntegrationDto($integrationData);
+                    $integration = $integrations->where('type', $integrationData['type'])->first();
+                    $this->merchantIntegrationService->updateIntegration($integration->id, $integrationDto);
+                }
+                return;
+            default:
+                //
+        }
+    }
+
+    private function saveSettings(array $data): void
+    {
+        switch ((int) $data['driver']) {
+            case ExtSystemDriver::DRIVER_MOY_SKLAD:
+                $this->merchantService->setSetting($data['merchantId'], self::MERCHANT_SETTING_PRICE_NAME, $data['settingPriceValue'] ?? '');
+                $this->merchantService->setSetting($data['merchantId'], self::MERCHANT_SETTING_ORGANIZATION_NAME, $data['settingOrganizationValue'] ?? '');
+                return;
+            default:
+                //
+        }
     }
 
     private function moyskladIntegrationsData(array $params): array
