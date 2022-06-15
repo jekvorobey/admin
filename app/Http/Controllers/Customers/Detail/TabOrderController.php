a40d884c72c4ba21cbd5631d5c5c69f8dd8b1aeb
Максим Купров <maxim.kyprov@ylab.io>
2022-06-15 10:47:46 +0300

    cleanup Customer TabOrderController

--- app/Http/Controllers/Customers/Detail/TabOrderController.php
+++ app/Http/Controllers/Customers/Detail/TabOrderController.php
@@ -4,65 +4,53 @@ namespace App\Http\Controllers\Customers\Detail;
 
 use App\Http\Controllers\Controller;
 use Greensight\CommonMsa\Dto\BlockDto;
-use Greensight\CommonMsa\Rest\RestQuery;
 use Greensight\Logistics\Dto\Lists\DeliveryMethod;
 use Greensight\Oms\Dto\Delivery\DeliveryDto;
 use Greensight\Oms\Dto\OrderDto;
-use Greensight\Oms\Dto\Payment\PaymentDto;
-use Greensight\Oms\Services\DeliveryService\DeliveryService;
 use Greensight\Logistics\Dto\Lists\DeliveryService as DeliveryServiceDto;
 use Greensight\Oms\Services\OrderService\OrderService;
-use Greensight\Oms\Services\PaymentService\PaymentService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Collection;
 
 class TabOrderController extends Controller
 {
-    public function load(
-        $id,
-        OrderService $orderService,
-        PaymentService $paymentService,
-        DeliveryService $deliveryService
-    ): JsonResponse {
+    public function load($id, OrderService $orderService): JsonResponse
+    {
         $this->canView(BlockDto::ADMIN_BLOCK_CLIENTS);
 
-        $orders = $orderService->orders((new RestQuery())->setFilter('customer_id', $id)->addSort('created_at', 'desc'));
-        if ($orders->count()) {
-            $orderIds = $orders->pluck('id')->all();
-            $payments = $paymentService->payments($orderIds)->groupBy('order_id');
+        $ordersQuery = $orderService->newQuery()
+            ->include('payments', 'paymentMethod', 'deliveries')
+            ->setFilter('customer_id', $id)
+            ->addSort('created_at', 'desc');
+        $orders = $orderService->orders($ordersQuery);
 
-            $deliveries = $deliveryService
-                ->deliveries((new RestQuery())->setFilter('order_id', $orderIds))
-                ->groupBy('order_id');
-
-            $orders = $orders->map(function (OrderDto $order) use ($payments, $deliveries) {
-                /** @var Collection|PaymentDto[] $ps */
-                $ps = $payments->get($order->id, collect());
-                /** @var Collection|DeliveryDto[] $ds */
-                $ds = $deliveries->get($order->id, collect());
-                $ar = $order->toArray();
-                $ar['status'] = $order->status();
-                $ar['isPayed'] = $order->isPayed();
-                $ar['deliveryType'] = $order->deliveryType();
-                $ar['paymentMethod'] = $ps->map(function (PaymentDto $payment) {
-                    return $payment->paymentMethod()->name;
-                })->unique()->join(', ');
-                $ar['deliveryMethod'] = $ds->map(function (DeliveryDto $delivery) {
-                    return DeliveryMethod::methodById($delivery->delivery_method)->name;
-                })->unique()->join(', ');
-                $ar['deliverySystems'] = $ds->map(function (DeliveryDto $delivery) {
-                    return DeliveryServiceDto::serviceById($delivery->delivery_service)->name;
-                })->unique()->join(', ');
-                $ar['deliveryCount'] = $ds->count();
-                $ar['deliveryDate'] = $ds->map(function (DeliveryDto $delivery) {
-                    return explode(' ', $delivery->delivery_at)[0];
-                })->unique()->join(', ');
-                return $ar;
-            });
+        if ($orders->isEmpty()) {
+            return response()->json([
+                'orders' => [],
+            ]);
         }
 
+        $result = $orders->map(function (OrderDto $order) {
+            $data = $order->toArray();
+            $data['status'] = $order->status();
+            $data['isPayed'] = $order->isPayed();
+            $data['deliveryType'] = $order->deliveryType();
+            $data['paymentMethod'] = $order->paymentMethod->name;
+            $data['deliveryMethod'] = $order->deliveries->map(function (DeliveryDto $delivery) {
+                return DeliveryMethod::methodById($delivery->delivery_method)->name;
+            })->unique()->join(', ');
+            $data['deliverySystems'] = $order->deliveries->map(function (DeliveryDto $delivery) {
+                return DeliveryServiceDto::serviceById($delivery->delivery_service)->name;
+            })->unique()->join(', ');
+            $data['deliveryCount'] = $order->deliveries->count();
+            $data['deliveryDate'] = $order->deliveries->map(function (DeliveryDto $delivery) {
+                return explode(' ', $delivery->delivery_at)[0];
+            })->unique()->join(', ');
+
+            return $data;
+        });
+
         return response()->json([
-            'orders' => $orders,
+            'orders' => $result,
         ]);
     }
 }
