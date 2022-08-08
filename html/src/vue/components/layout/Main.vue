8f228e9b88613b21bf4258a85160fba6888142c9
Alexander <aleksandr-shiryaev-1993@mail.ru>
2022-08-08 10:34:48 +0300

    fix width header

--- html/src/vue/components/layout/Main.vue
+++ html/src/vue/components/layout/Main.vue
@@ -3,7 +3,7 @@
         <LayoutHeader :on-index="onIndex"></LayoutHeader>
         <div class="container-fluid">
             <div class="row flex-xl-nowrap">
-                <div class="bg-light col-xl-2 no-padding w-20" v-if="!user.isGuest">
+                <div class="bg-light col-xl-2 no-padding mw-250" v-if="!user.isGuest">
                     <MainMenu></MainMenu>
                 </div>
                 <main class="flex-grow-1 no-padding" :class="!user.isGuest ? 'col-xl-10' : 'col-xl-12'">
@@ -88,8 +88,8 @@
 </script>
 
 <style scoped>
-    .w-20{
-        min-width: 20%;
+    .mw-250{
+        min-width: 250px;
     }
     .fake-vue-body {
         height: 100%;
