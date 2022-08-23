d00826cd9ea8fd626f2694b215c3c6051627cc65
Александр Романенко <romanenko.a@bk.ru>
2022-08-23 08:21:12 +0300

    BX-7042: update modal form

--- html/src/vue/pages/Order/Detail/components/forms/modal-credit-payment-receipt-create.vue
+++ html/src/vue/pages/Order/Detail/components/forms/modal-credit-payment-receipt-create.vue
@@ -4,11 +4,14 @@
             <b-form-row>
                 <div class="col-12 text-center">
                     <button class="btn btn-info mb-1 w-75" @click="createCreditPaymentReceipt(1)">Расчёт "ПРЕДОПЛАТА"</button>
+                    <p class="text-left">Формирование кассового чека с расчетом "Предоплата", если заказ в статусе "Передан в доставку", статус кредитной заявки сменился на "Cached" и нет ранее сформированных чеков</p>
                     <br>
                     <button class="btn btn-info mb-1 w-75" @click="createCreditPaymentReceipt(2)">Расчёт "В КРЕДИТ"</button>
+                    <p class="text-left">Формирование кассового чека с расчетом "В кредит", если заказ в статусе "Передан в доставку" и нет ранее сформированных чеков</p>
                     <br>
                     <button class="btn btn-info mb-1 w-75" @click="createCreditPaymentReceipt(3)">Расчёт "ПОГАШЕНИЕ КРЕДИТА"</button>
-                </div>
+                    <p class="text-left">Формирование кассового чека с расчетом "Погашение кредита", если статус кредитной заявки сменился на "Cached", заказ еще в пути и ранее был выбит чек с расчетом "В кредит"</p>
+                    </div>
             </b-form-row>
             <div class="float-right mt-3">
                 <b-button @click="close()" variant="outline-primary">Отмена</b-button>
