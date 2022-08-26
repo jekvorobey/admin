cd358cf9fa982f7edbbf6bab6cec9df3223fac23
Leonid <l.borisov@ibt.ru>
2022-08-26 09:42:27 +0300

    BX-7618: fix offer_id filter

--- app/Http/Controllers/Product/ProductSelectionController.php
+++ app/Http/Controllers/Product/ProductSelectionController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers\Product;
 use App\Http\Controllers\Controller;
 use Greensight\CommonMsa\Dto\BlockDto;
 use Greensight\CommonMsa\Dto\RoleDto;
-use Greensight\CommonMsa\Rest\RestQuery;
 use Illuminate\Http\Request;
 use Pim\Core\PimException;
 use Pim\Dto\Search\ProductQuery;
-use Pim\Services\BrandService\BrandService;
-use Pim\Services\OfferService\OfferService;
 use Pim\Services\SearchService\SearchService;
 
 class ProductSelectionController extends Controller
@@ -18,34 +15,13 @@ class ProductSelectionController extends Controller
     /**
      * @throws PimException
      */
-    public function selection(
-        Request $request,
-        SearchService $searchService,
-        BrandService $brandService,
-        OfferService $offerService
-    ) {
+    public function selection(Request $request, SearchService $searchService)
+    {
         $this->canView(BlockDto::ADMIN_BLOCK_PRODUCTS);
         $this->title = 'Подбор товаров';
 
         $query = $this->makeQuery($request);
         $productSearchResult = $searchService->products($query);
-
-        $productIds = collect($productSearchResult->products)->pluck('id')->all();
-
-        $offers = $offerService->offers(
-            (new RestQuery())
-                ->setFilter('product_id', $productIds)
-        );
-
-        $offers = $offers->mapToGroups(function ($item) {
-            return [$item->product_id => $item->id];
-        });
-
-        $productSearchResult->products = array_map(function ($product) use ($offers) {
-
-            $product['offerIds'] = $offers[$product['id']] ?? null;
-            return $product;
-        }, $productSearchResult->products);
 
         dump([
             'iProducts' => $productSearchResult->products,
@@ -53,7 +29,6 @@ class ProductSelectionController extends Controller
             'iCurrentPage' => $request->get('page', 1),
             'iFilter' => $request->get('filter', []),
             'options' => [
-                'brands' => $brandService->brands($brandService->newQuery())->values()->toArray(),
                 'merchants' => $this->getMerchants()->values(),
             ],
         ]);
@@ -78,12 +53,9 @@ class ProductSelectionController extends Controller
         ]);
 
         $filter = $request->get('filter', []);
+        $query->offer_id = data_get($filter, 'offerId');
         $query->merchantId = data_get($filter, 'merchants');
         $query->vendorCode = data_get($filter, 'vendorCode');
-        $query->id = data_get($filter, 'id');
-        $query->name = data_get($filter, 'name');
-        $query->brandCode = data_get($filter, 'brand');
-        $query->badges = data_get($filter, 'badges');
         $query->orderBy(ProductQuery::DATE_ADD, 'desc');
 
         return $query;
