92f10f5de9498ccb95d9fa43e9c3531b4280ea2c
Alexander <aleksandr-shiryaev-1993@mail.ru>
2022-10-04 17:29:29 +0300

    BX-7531: fix z-index

--- html/src/vue/components/filter/f-custom-search-select.vue
+++ html/src/vue/components/filter/f-custom-search-select.vue
@@ -144,7 +144,7 @@
         font-size: 16px;
         border: 1px solid gainsboro;
         position: absolute;
-        z-index: 1;
+        z-index: 10;
         background: #fff;
     }
     .aselect li {
