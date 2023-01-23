4a341b6117e5925cba3f7d32fc5e96d90f4c6b93
Leonid <l.borisov@ibt.ru>
2023-01-23 19:15:01 +0400

    10899: add xmlId and guid to price change front

--- html/src/vue/pages/Claim/PriceChange/Detail/components/offers-tab.vue
+++ html/src/vue/pages/Claim/PriceChange/Detail/components/offers-tab.vue
@@ -3,176 +3,199 @@
         <div class="shadow mt-3 mr-3 p-3">
             <table class="table table-condensed">
                 <thead>
-                    <tr>
-                        <th>ID предложения мерчанта</th>
-                        <th>ID товара</th>
-                        <th>Название товара</th>
-                        <th>Артикул товара</th>
-                        <th>Старая цена предложения мерчанта</th>
-                        <th>Новая цена предложения мерчанта</th>
-                        <th>Отклонение</th>
-                        <th>Статус изменения цены</th>
-                        <th v-if="canUpdate(blocks.claims)">Комментарий по изменению цены</th>
-                        <th>
-                            <template v-if="!claim.isProcessed">
-                                <button class="btn btn-primary" v-if="isWorkStatus && canUpdate(blocks.claims)"
-                                        @click="changePrice('accept')"
-                                        title="Принять все необработанные изменения">Принять все</button><br>
-                                <button class="btn btn-primary mt-1" v-if="isWorkStatus && canUpdate(blocks.claims)"
-                                        @click="rejectPrice()"
-                                        title="Отклонить все необработанные изменения">Отклонить все</button>
-                            </template>
-                        </th>
-                    </tr>
+                <tr>
+                    <th>ID предложения мерчанта</th>
+                    <th>ID товара</th>
+                    <th>Название товара</th>
+                    <th>Артикул товара</th>
+                    <th>Старая цена предложения мерчанта</th>
+                    <th>Новая цена предложения мерчанта</th>
+                    <th>Отклонение</th>
+                    <th>Статус изменения цены</th>
+                    <th v-if="canUpdate(blocks.claims)">Комментарий по изменению цены</th>
+                    <th>
+                        <template v-if="!claim.isProcessed">
+                            <button class="btn btn-primary" v-if="isWorkStatus && canUpdate(blocks.claims)"
+                                    @click="changePrice('accept')"
+                                    title="Принять все необработанные изменения">Принять все
+                            </button>
+                            <br>
+                            <button class="btn btn-primary mt-1" v-if="isWorkStatus && canUpdate(blocks.claims)"
+                                    @click="rejectPrice()"
+                                    title="Отклонить все необработанные изменения">Отклонить все
+                            </button>
+                        </template>
+                    </th>
+                </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="offer in claim.payload.offers">
-                        <td>
-                            <div>{{ offer.offerId }}</div>
-                            <div v-if="model && model.productsByOffers">{{ getXmlID(offer.offerId) }}</div>
-                        </td>
-                        <td>{{ product(offer.offerId).id }}</td>
-                        <td v-if="canView(blocks.products)">
-                            <a :href="getRoute('products.detail', {id: product(offer.offerId).id})" target="_blank">
-                                {{ product(offer.offerId).name }}
-                            </a>
-                        </td>
-                        <td v-else>
+                <tr v-for="offer in claim.payload.offers">
+                    <td>
+                        <div>{{ offer.offerId }}</div>
+                        <div v-if="model && model.productsByOffers">{{ getXmlID(offer.offerId) }}</div>
+                    </td>
+                    <td>{{ product(offer.offerId).id }}</td>
+                    <td v-if="canView(blocks.products)">
+                        <a :href="getRoute('products.detail', {id: product(offer.offerId).id})" target="_blank">
                             {{ product(offer.offerId).name }}
-                        </td>
-                        <td>{{ product(offer.offerId).vendor_code }}</td>
-                        <td>{{ offer.oldPrice }} руб</td>
-                        <td>{{ offer.newPrice }} руб</td>
-                        <td>
-                            <value-difference
-                                v-if="offer.oldPrice > 0"
-                                :old-value="offer.oldPrice"
-                                :new-value="offer.newPrice"
-                            />
-                        </td>
-                        <td>
+                        </a>
+                    </td>
+                    <td v-else>
+                        {{ product(offer.offerId).name }}
+                    </td>
+                    <td>{{ product(offer.offerId).vendor_code }}</td>
+                    <td>{{ offer.oldPrice }} руб</td>
+                    <td>{{ offer.newPrice }} руб</td>
+                    <td>
+                        <value-difference
+                            v-if="offer.oldPrice > 0"
+                            :old-value="offer.oldPrice"
+                            :new-value="offer.newPrice"
+                        />
+                    </td>
+                    <td>
                             <span class="badge" :class="statusChangePriceClass(offer.status)">
-                                {{ statusChangePrice(offer.status) }}<br>{{offer.updated_at}}
+                                {{ statusChangePrice(offer.status) }}<br>{{ offer.updated_at }}
                             </span>
-                        </td>
-                        <td>{{ offer.comment }}</td>
-                        <td v-if="canUpdate(blocks.claims)">
-                            <template v-if="!offer.status">
-                                <button class="btn btn-primary" v-if="isWorkStatus"
-                                        @click="changePrice('accept', offer.offerId)">Принять</button><br>
-                                <button class="btn btn-primary mt-1" v-if="isWorkStatus"
-                                        @click="rejectPrice(offer.offerId)">Отклонить</button>
-                            </template>
-                        </td>
-                    </tr>
-                    <tr v-if="!claim.payload.offers">
-                        <td colspan="7">Предложений мерчанта нет</td>
-                    </tr>
+                    </td>
+                    <td>{{ offer.comment }}</td>
+                    <td v-if="canUpdate(blocks.claims)">
+                        <template v-if="!offer.status">
+                            <button class="btn btn-primary" v-if="isWorkStatus"
+                                    @click="changePrice('accept', offer.offerId)">Принять
+                            </button>
+                            <br>
+                            <button class="btn btn-primary mt-1" v-if="isWorkStatus"
+                                    @click="rejectPrice(offer.offerId)">Отклонить
+                            </button>
+                        </template>
+                    </td>
+                </tr>
+                <tr v-if="!claim.payload.offers">
+                    <td colspan="7">Предложений мерчанта нет</td>
+                </tr>
                 </tbody>
             </table>
         </div>
 
         <offer-price-reject-modal
-                :offer-id="selectedOffer"
-                @onSave="rejectPriceSave"
-                modal-name="offerPriceReject">
+            :offer-id="selectedOffer"
+            @onSave="rejectPriceSave"
+            modal-name="offerPriceReject">
         </offer-price-reject-modal>
     </div>
 </template>
 
 <script>
-    import Services from '../../../../../../scripts/services/services';
-    import OfferPriceRejectModal from './offer-price-reject-modal.vue';
-    import modalMixin from '../../../../../mixins/modal';
-    import ValueDifference from '../../../../../components/value-difference/value-difference.vue';
+import Services from '../../../../../../scripts/services/services';
+import OfferPriceRejectModal from './offer-price-reject-modal.vue';
+import modalMixin from '../../../../../mixins/modal';
+import ValueDifference from '../../../../../components/value-difference/value-difference.vue';
 
-    export default {
-        components: {
-            ValueDifference,
-            OfferPriceRejectModal
-        },
-        props: [
-            'model',
-        ],
-        data() {
-            return {
-                'selectedOffer': 0,
+export default {
+    components: {
+        ValueDifference,
+        OfferPriceRejectModal
+    },
+    props: [
+        'model',
+    ],
+    data() {
+        return {
+            'selectedOffer': 0,
+        }
+    },
+    mixins: [modalMixin],
+    methods: {
+        getXmlID(offerId) {
+            try {
+                return this.model.productsByOffers[offerId].offer.xml_id
+            } catch (e) {
+                return null
             }
         },
-        mixins: [modalMixin],
-        methods: {
-            getXmlID(offerId) {
-                try {
-                    return this.model.productsByOffers[offerId].offer.xml_id
-                } catch (e) {
-                    return null
-                }
-            },
-            isStatus(statusId) {
-                return this.claim.status === statusId;
-            },
-            product(offerId) {
-                return this.claim.productsByOffers[offerId].product;
-            },
-            statusChangePriceClass(status) {
-                switch (status) {
-                    case 'accept': return 'badge-success';
-                    case 'reject': return 'badge-warning';
-                    default: return 'badge-secondary';
-                }
-            },
-            statusChangePrice(status) {
-                switch (status) {
-                    case 'accept': return 'Изменена';
-                    case 'reject': return 'Не изменена';
-                    default: return 'Нет решения';
-                }
-            },
-            rejectPrice(offerId = 0) {
-                this.selectedOffer = offerId;
-                this.openModal('offerPriceReject');
-            },
-            rejectPriceSave(data) {
-                this.changePrice('reject', data.offerId, data.comment);
-            },
-            changePrice(action, offerId = 0, comment = '') {
-                let errorMessage = 'Ошибка при изменении ' + (offerId ? 'цены.' : 'цен.');
-                let data = {'action': action};
-                if (offerId) {
-                    data['offerId'] = offerId;
-                }
-                if (comment) {
-                    data['comment'] = comment;
-                }
+        getGuid(offerId) {
+            try {
+                return this.model.productsByOffers[offerId].offer.guid
+            } catch (e) {
+                return null
+            }
+        },
+        isStatus(statusId) {
+            return this.claim.status === statusId;
+        },
+        product(offerId) {
+            return this.claim.productsByOffers[offerId].product;
+        },
+        statusChangePriceClass(status) {
+            switch (status) {
+                case 'accept':
+                    return 'badge-success';
+                case 'reject':
+                    return 'badge-warning';
+                default:
+                    return 'badge-secondary';
+            }
+        },
+        statusChangePrice(status) {
+            switch (status) {
+                case 'accept':
+                    return 'Изменена';
+                case 'reject':
+                    return 'Не изменена';
+                default:
+                    return 'Нет решения';
+            }
+        },
+        rejectPrice(offerId = 0) {
+            this.selectedOffer = offerId;
+            this.openModal('offerPriceReject');
+        },
+        rejectPriceSave(data) {
+            this.changePrice('reject', data.offerId, data.comment);
+        },
+        changePrice(action, offerId = 0, comment = '') {
+            let errorMessage = 'Ошибка при изменении ' + (offerId ? 'цены.' : 'цен.');
+            let data = {'action': action};
+            if (offerId) {
+                data['offerId'] = offerId;
+            }
+            if (comment) {
+                data['comment'] = comment;
+            }
 
-                Services.showLoader();
-                Services.net().put(this.getRoute('priceChangeClaims.changePrice', {id: this.claim.id}), null, data).then(response => {
-                    if (response.result === 'ok') {
-                        this.claim.payload = response.claimPayload;
-                        Services.msg("Изменения сохранены");
-                    } else {
-                        Services.msg(errorMessage + ' ' + response.error, 'danger');
-                    }
-                }, () => {
-                    Services.msg(errorMessage, 'danger');
-                }).finally(() => {
-                    Services.hideLoader();
-                });
+            Services.showLoader();
+            Services.net().put(this.getRoute('priceChangeClaims.changePrice', {id: this.claim.id}), null, data).then(response => {
+                if (response.result === 'ok') {
+                    this.claim.payload = response.claimPayload;
+                    Services.msg("Изменения сохранены");
+                } else {
+                    Services.msg(errorMessage + ' ' + response.error, 'danger');
+                }
+            }, () => {
+                Services.msg(errorMessage, 'danger');
+            }).finally(() => {
+                Services.hideLoader();
+            });
+        },
+    },
+    computed: {
+        claim: {
+            get() {
+                return this.model
+            },
+            set(value) {
+                this.$emit('update:model', value)
             },
         },
-        computed: {
-            claim: {
-                get() {return this.model},
-                set(value) {this.$emit('update:model', value)},
-            },
-            isWorkStatus() {
-                return this.isStatus(2);
-            },
+        isWorkStatus() {
+            return this.isStatus(2);
         },
+    },
 };
 </script>
 <style scoped>
-    th {
-        vertical-align: top !important;
-    }
+th {
+    vertical-align: top !important;
+}
 </style>
